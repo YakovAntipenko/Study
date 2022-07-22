939a78cf2fc9ab87a6b04a4dc683f9b13f884dce
YakovAntipenko <yashock1234@gmail.com>
2022-07-22 12:09:48 +0300

    added Terraform-1

---
+++ Terraform-1/terraform-aws-instance-5/main.tf
@@ -0,0 +1,31 @@
+terraform {
+    cloud {
+    organization = "YakovAntipenko"
+    workspaces {
+      name = "Test-Workspace"
+    }
+  }
+
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "test_server" {
+  ami           = "ami-0cff7528ff583bf9a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
